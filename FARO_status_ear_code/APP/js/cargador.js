1d327e0960f7e016c42764c1fbc4c7d1eafb80da
martinsantome <martinsantome@openmailbox.org>
2015-06-25 15:00:43 +0200

    Add loader progress bar animation

--- FARO_status_ear_code/APP/js/cargador.js
+++ FARO_status_ear_code/APP/js/cargador.js
@@ -1,5 +1,28 @@
+
 function cargar_ejemplo( id, contenedor ) {
 
-	$("#" + contenedor).load( "./samples/sample" + id + "/index.html" );
+    function poner_loading( valor=true) {
+            
+       if (valor) {
+            $("#" + contenedor + "_bar_img").show();
+        } else {
+            $("#" + contenedor + "_bar_img").hide();
+        }
+    }
+    
+    poner_loading(true);
+    
+        
+    $("#" + contenedor).load( "./samples/sample" + id + "/index.html",
+        function(){ 
+            poner_loading(false);
+           } 
+    );
 
+}
+
+function esperar(contenedor) {
+    $("#" + contenedor + "_bar_img").hide();
+    
+    console.log($("#" + contenedor + "_bar_img"));
 }
